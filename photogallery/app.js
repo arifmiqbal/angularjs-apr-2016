4e4b1b0b26946bc3f2f1c2cbcbdf9df328075685
Arif_Iqbal <arif.iqbal@avon.com>
2016-04-18 15:21:12 -0400

    photogallery

--- photogallery/app.js
+++ photogallery/app.js
@@ -1,10 +1,14 @@
 'use strict';
 
-angular.module('example366', ['ngAnimate', 'ngTouch'])
-  .controller('MainCtrl', function ($scope) {
+
+  var myMod = angular.module('example366', ['ngAnimate', 'ngTouch']);
+
+  myMod.controller('MainCtrl', function () {
+
+    var self = this;
 
     // Set of Photos
-    $scope.photos = [
+    self.photos = [
         {src: 'http://farm9.staticflickr.com/8042/7918423710_e6dd168d7c_b.jpg', desc: 'Image 01'},
         {src: 'http://farm9.staticflickr.com/8449/7918424278_4835c85e7a_b.jpg', desc: 'Image 02'},
         {src: 'http://farm9.staticflickr.com/8457/7918424412_bb641455c7_b.jpg', desc: 'Image 03'},
@@ -14,25 +18,26 @@ angular.module('example366', ['ngAnimate', 'ngTouch'])
     ];
 
     // initial image index
-    $scope._Index = 0;
+    self._Index = 0;
 
     // if a current image is the same as requested image
-    $scope.isActive = function (index) {
-        return $scope._Index === index;
+    self.isActive = function (index) {
+        return self._Index === index;
     };
 
     // show prev image
-    $scope.showPrev = function () {
-        $scope._Index = ($scope._Index > 0) ? --$scope._Index : $scope.photos.length - 1;
+    self.showPrev = function () {
+        console.log(self._Index +' || '+--self._Index);
+        self._Index = (self._Index > 0) ? --self._Index : self.photos.length - 1;
     };
 
     // show next image
-    $scope.showNext = function () {
-        $scope._Index = ($scope._Index < $scope.photos.length - 1) ? ++$scope._Index : 0;
+    self.showNext = function () {
+        self._Index = (self._Index < self.photos.length - 1) ? ++self._Index : 0;
     };
 
     // show a certain image
-    $scope.showPhoto = function (index) {
-        $scope._Index = index;
+    self.showPhoto = function (index) {
+        self._Index = index;
     };
 });
