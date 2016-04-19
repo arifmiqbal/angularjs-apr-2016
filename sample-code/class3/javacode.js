08efef3325db257144486459ba2e5c0df9806b22
Arif Iqbal/NYC/US <arif.iqbal@avon.com>
2016-04-18 21:23:48 -0400

    workshop and class task

---
+++ sample-code/class3/javacode.js
@@ -0,0 +1,80 @@
+
+var myMod = angular.module('MyModule', []);
+
+myMod.controller('MyController', function(){
+
+	var self = this;
+
+	self.pgtitle = 'Assignment #1 - UI Theme Picker';
+
+	self.isBoxChecked = true;
+
+	self.onUserClick = function(){
+
+		console.log('you click a button');
+		self.isBoxChecked = !self.isBoxChecked;
+	}
+
+	self.default = {
+		colorTitle: 'Default',
+		bkgColor: 'whiteBkg',
+		box1Color: 'whiteBkgBox1' ,
+		box1textColor: 'grey' ,
+		box1Text: 'Default Theme Box 1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+		box2Color: 'whiteBkgBox2' ,
+		box2textColor: 'grey',
+		box2Text: 'Default Theme Box 2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+	};
+
+
+	self.redColor = {
+		colorTitle: 'Red',
+		bkgColor: 'redBkg',
+		box1Color: 'redBkgBox1',
+		box1textColor: 'whiteText',
+		box1Text:'Red Theme Box 1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+		box2Color: 'redBkgBox2' ,
+		box2textColor: 'whiteText',
+		box2Text:'Red Theme Box 2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+
+	};
+
+	self.greenColor = {
+		colorTitle: 'Green',
+		bkgColor: 'greenBkg',
+		box1Color: 'greenBkgBox1' ,
+		box1textColor: 'black' ,
+		box1Text:'Green Theme Box 1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+		box2Color: 'greenBkgBox2' ,
+		box2textColor: 'black',
+		box2Text:'Green Theme Box 2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+	};
+
+	self.blueColor = {
+		colorTitle: 'Blue',
+		bkgColor: 'blueBkg',
+		box1Color: 'blueBkgBox1' ,
+		box1textColor: 'white' ,
+		box1Text:'Blue Theme Box 1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
+		box2Color: 'blueBkgBox2' ,
+		box2textColor: 'white',
+		box2Text:'Blue Theme Box 2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+	};
+
+	self.yellowColor = {
+		colorTitle: 'Yellow',
+		bkgColor: 'yellowBkg',
+		box1Color: 'yellowBkgBox1' ,
+		box1textColor: 'blackText' ,
+		box1Text:'Yellow Theme Box 1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+		box2Color: 'yellowBkgBox2' ,
+		box2textColor: 'blackText',
+		box2Text:'Yellow Theme Box 2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+	};
+
+	self.colorSelected = self.default;
+
+
+
+	
+});
---
+++ sample-code/class3/workshop/javacode.js
@@ -0,0 +1,47 @@
+
+var myMod = angular.module('MyModule', []);
+
+myMod.controller('MyController', function(){
+
+	var self = this;
+
+	self.isBoxChecked = true;
+
+	self.onUserClick = function(){
+
+		console.log('you click a button');
+		self.isBoxChecked = !self.isBoxChecked;
+		
+	}
+
+
+	self.onDoneClick = function(){
+
+		console.log('you click a button');
+		self.isBoxChecked = !self.isBoxChecked;
+	}
+
+
+	self.onCancelClick = function(){
+
+		console.log('you click a button');
+		self.isBoxChecked = !self.isBoxChecked;
+		self.userProfile = angular.copy(self.userDefaultProfile);
+	}
+	
+
+	self.userProfile = {
+		firstname: 'George',
+		lastname: 'Dagher',
+		email: 'george@nycda.com' 
+	};
+
+	self.userDefaultProfile = angular.copy(self.userProfile);
+
+
+	
+
+
+
+	
+});
