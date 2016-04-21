5da8112553e6ad8eef6635e406f72deded50e3f3
Arif Iqbal/NYC/US <arif.iqbal@avon.com>
2016-04-21 16:56:58 -0400

    Signed-off-by: Arif Iqbal/NYC/US <arif.iqbal@avon.com>

---
+++ assignment2/javacode.js
@@ -0,0 +1,92 @@
+
+var myMod = angular.module('MyModule', []);
+
+
+myMod.controller('MyFolderController', function(){
+
+	var self = this;
+
+	self.isBoxChecked = false;
+
+	self.filename = '';
+	
+	self.folders = [{
+
+		foldername:"Folder 1",
+		subfoldername: ['folder 1.1', 'folder 1.2', 'folder 1.3']
+
+	},
+	{
+		foldername:"Folder 2",
+		subfoldername: ['folder 2.1', 'folder 2.2', 'folder 2.3']
+
+	},
+	{
+		foldername:"Folder 3",
+		subfoldername: ['folder 3.1', 'folder 3.2', 'folder 3.3']
+
+	},
+	{
+		foldername:"Folder 4",
+		subfoldername: ['folder 4.1', 'folder 4.2', 'folder 4.3']
+
+	}];
+
+
+	//Add Folder
+
+	self.onUserClickFolder = function(){
+
+		self.isBoxChecked = true;
+		console.log(self.folderSelected + 'dddd');
+		//self.folders[self.folderSelected][1].push(self.folders.subfoldername);
+
+		self.folders[self.folderSelected].subfoldername.push(self.folders.subfoldername); 
+		
+		self.folders.subfoldername ='';
+		self.folderSelected = null;
+
+		console.log('you click a button');
+	};
+
+	
+});
+
+
+myMod.controller('MyFormController', function(){
+
+	var self = this;
+
+	self.userForm = [];
+	
+	// Add item to array and clear the fields
+
+	self.onUserClick = function(){
+
+		self.userForm.push({
+
+			name: self.userForm.name,
+			description: self.userForm.description,
+			priority: self.userForm.priority
+	
+		});
+
+		self.userForm.name ='';
+		self.userForm.description ='';
+		self.userForm.priority ='';
+
+		console.log('you click a button');
+	};
+
+	//Remove item
+
+	self.remove = function(index) {
+		console.log(index);
+    	self.userForm.splice(index, 1);
+    };
+
+
+});
+
+
+
---
+++ sample-code/class4/javacode2.js
@@ -0,0 +1,25 @@
+angular.module('MyModule', [])
+
+.controller('MyController', function($timeout, $interval, $log){
+
+	var self = this;
+
+	$log.log('normal log');
+	$log.info('info log');
+	$log.error('error log');
+	$log.debug('debug log');
+
+	self.name = "Harry";
+
+	$timeout(function(){
+		console.log('function was called');
+		self.name = "George";
+	}, 3000);
+
+	self.count = 10;
+	$interval(function(){
+		console.log('function was called');
+		self.count--;
+	}, 1000, 10);
+	
+});
